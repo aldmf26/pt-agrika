efb1f139c4b8f2eae6e8d86da2c1596ed7633bce
aldmf26 <aldimf26@gmail.com>
2025-03-21 08:00:36 +0800

    dsa

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -43,6 +43,7 @@
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 
 </head>
 
