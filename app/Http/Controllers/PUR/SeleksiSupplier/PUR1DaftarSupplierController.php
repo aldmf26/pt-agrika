b2a2d5be4ed0de255f376997beb1dbc9e65d9b7c
aldmf26 <aldimf26@gmail.com>
2025-05-07 11:59:28 +0800

    update

--- app/Http/Controllers/PUR/SeleksiSupplier/PUR1DaftarSupplierController.php
+++ app/Http/Controllers/PUR/SeleksiSupplier/PUR1DaftarSupplierController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\PUR\SeleksiSupplier;
 
 use App\Http\Controllers\Controller;
+use App\Models\PurchaseOrder;
 use App\Models\Suplier;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -29,6 +30,16 @@ class PUR1DaftarSupplierController extends Controller
         return view('pur.seleksi.daftar_supplier.create', $data);
     }
 
+    public function edit($id)
+    {
+        $data = [
+            'title' => 'Edit Daftar Supplier',
+            'supplier' => Suplier::findOrFail($id)->first(),
+        ];
+
+        return view('pur.seleksi.daftar_supplier.edit', $data);
+    }
+
     public function store(Request $r)
     {
         DB::beginTransaction();
@@ -53,7 +64,42 @@ class PUR1DaftarSupplierController extends Controller
             return redirect()->route('pur.seleksi.1.create')->with('error', $e->getMessage());
         }
     }
+    public function update(Request $r, $id)
+    {
+        DB::beginTransaction();
 
+        try {
+            Suplier::find($id)->update([
+                'nama_supplier' => $r->nama_supplier,
+                'kategori' => $r->jenis_produk,
+                'alamat' => $r->alamat_supplier,
+                'produsen' => $r->produsen,
+                'contact_person' => $r->contact_person,
+                'no_telp' => $r->no_telp,
+                'ket' => $r->ket,
+                'hasil_evaluasi' => $r->hasil,
+            ]);
+
+            DB::commit();
+
+            return redirect()->route('pur.seleksi.1.index')->with('sukses', 'Seleksi Supplier berhasil diupdate');
+        } catch (\Exception $e) {
+            DB::rollback();
+            return redirect()->route('pur.seleksi.1.create')->with('error', $e->getMessage());
+        }
+    }
+    public function destroy($id)
+    {
+        $supplier = Suplier::findOrFail($id);
+        $check = PurchaseOrder::where('supplier', $supplier->nama_supplier)->first();
+        if ($check) {
+            return redirect()->route('pur.seleksi.1.index')->with('error', 'Gagal menghapus, supplier masih digunakan di Purchase Order');
+        } else {
+            $supplier->delete();
+        }
+
+        return redirect()->route('pur.seleksi.1.index')->with('sukses', 'Seleksi Supplier berhasil dihapus');
+    }
     public function print()
     {
         $datas = Suplier::latest()->get();
--- app/Models/ProgramAuditInternal.php
+++ app/Models/ProgramAuditInternal.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class ProgramAuditInternal extends Model
 {
     protected $guarded = [];
+
+    public function getDepartemenAttribute($value)
+    {
+        return strtoupper($value);
+    }
+    public function getAuditeAttribute($value)
+    {
+        return ucfirst($value);
+    }
+    public function getAuditorAttribute($value)
+    {
+        return ucfirst($value);
+    }
 }
