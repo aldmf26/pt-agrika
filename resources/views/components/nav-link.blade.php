8db0d3dacf722ee12896e4a1ac8ff2580cdc0df9
aldmf26 <aldimf26@gmail.com>
2025-06-23 10:53:14 +0800

    dsa

--- resources/views/components/nav-link.blade.php
+++ resources/views/components/nav-link.blade.php
@@ -1,16 +1,15 @@
 @php
-    $rot = request()->route()->getName();
     $k = request()->get('k');
 @endphp
+@props(['route' => 'ppc.gudang-rm.5.index'])
 <ul class="nav nav-pills float-start">
     <li class="nav-item">
-        <a class="nav-link  {{ !$k ? 'active' : '' }}" aria-current="page"
-            href="{{ route('ppc.gudang-rm.5.index') }}">Barang & Kemasan</a>
+        <a class="nav-link  {{ !$k ? 'active' : '' }}" aria-current="page" href="{{ route($route) }}">Barang & Kemasan</a>
     </li>
 
     <li class="nav-item">
         <a class="nav-link  {{ $k == 'sbw' ? 'active' : '' }}" aria-current="page"
-            href="{{ route('ppc.gudang-rm.5.index', ['k' => 'sbw']) }}">SBW</a>
+            href="{{ route($route, ['k' => 'sbw']) }}">SBW</a>
     </li>
 
 </ul>
