a926b66cff382535bf26aa62752f12c4bec7996d
nadws <nandw567@gmail.com>
2025-07-02 12:00:40 +0800

    update PUR1DaftarSupplierController.php, resources/views/pur/seleksi/daftar_supplier/index.blade.php and print.blade.php

--- resources/views/pur/seleksi/daftar_supplier/index.blade.php
+++ resources/views/pur/seleksi/daftar_supplier/index.blade.php
@@ -1,11 +1,14 @@
 <x-app-layout :title="$title">
+    <x-nav-link route="pur.seleksi.1.index" />
+    <br>
     <div class="d-flex justify-content-end gap-2">
         <div>
 
         </div>
         <div>
 
-            <a href="{{ route('pur.seleksi.1.print') }}" class="btn btn-sm btn-primary"><i class="fas fa-print"></i>
+            <a href="{{ route('pur.seleksi.1.print', ['kategori' => $k]) }}" class="btn btn-sm btn-primary"><i
+                    class="fas fa-print"></i>
                 Print</a>
 
             <a href="{{ route('pur.seleksi.1.create') }}" class="btn btn-sm btn-primary"><i class="fas fa-plus"></i>
@@ -29,34 +32,59 @@
             </tr>
         </thead>
         <tbody>
-            @foreach ($datas as $d)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $d->nama_supplier }}</td>
-                    <td>{{ $d->alamat }}</td>
-                    <td>{{ $d->produsen }}</td>
-                    <td>{{ $d->contact_person }}</td>
-                    <td>{{ $d->no_telp }}</td>
-                    <td>{{ $d->kategori }}</td>
-                    <td>
-                        @foreach ($d->evaluasi as $evaluasi)
-                            <a href="{{ route('pur.seleksi.1.evaluasi', $d->id) }}"
-                                class="btn btn-xs btn-primary">evaluasi Bulan {{ $evaluasi->periode_evaluasi }}</a>
-                        @endforeach
-                    </td>
-                    <td>{{ $d->ket }}</td>
-                    <td>
+            @if ($k == 'satu')
+                @foreach ($datas as $d)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $d->nama_supplier }}</td>
+                        <td>{{ $d->alamat }}</td>
+                        <td>{{ $d->produsen }}</td>
+                        <td>{{ $d->contact_person }}</td>
+                        <td>{{ $d->no_telp }}</td>
+                        <td>{{ $d->kategori }}</td>
+                        <td>
+                            @foreach ($d->evaluasi as $evaluasi)
+                                <a href="{{ route('pur.seleksi.1.evaluasi', $d->id) }}"
+                                    class="btn btn-xs btn-primary">evaluasi Bulan {{ $evaluasi->periode_evaluasi }}</a>
+                            @endforeach
+                        </td>
+                        <td>{{ $d->ket }}</td>
+                        <td>
 
 
-                        <a href="{{ route('pur.seleksi.1.edit', $d->id) }}" class="btn btn-xs btn-primary"><i
-                                class="fas fa-edit"></i></a>
+                            <a href="{{ route('pur.seleksi.1.edit', $d->id) }}" class="btn btn-xs btn-primary"><i
+                                    class="fas fa-edit"></i></a>
 
-                        <a onclick="return confirm('Yakin ingin menghapus data ini?')"
-                            href="{{ route('pur.seleksi.1.destroy', $d->id) }}" class="btn btn-xs btn-danger"><i
-                                class="fas fa-trash"></i></a>
-                    </td>
-                </tr>
-            @endforeach
+                            <a onclick="return confirm('Yakin ingin menghapus data ini?')"
+                                href="{{ route('pur.seleksi.1.destroy', $d->id) }}" class="btn btn-xs btn-danger"><i
+                                    class="fas fa-trash"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
+                @foreach ($rumah_walet as $r)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $r->nama }}</td>
+                        <td>{{ $r->alamat }}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>Supllier material SBW</td>
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <a href="{{ route('pur.seleksi.1.edit', $r->id) }}" class="btn btn-xs btn-primary"><i
+                                    class="fas fa-edit"></i></a>
+                            <a onclick="return confirm('Yakin ingin menghapus data ini?')"
+                                href="{{ route('pur.seleksi.1.destroy', $r->id) }}" class="btn btn-xs btn-danger"><i
+                                    class="fas fa-trash"></i></a>
+                        </td>
+                    </tr>
+                @endforeach
+            @endif
+
+
         </tbody>
     </table>
 
--- resources/views/pur/seleksi/daftar_supplier/print.blade.php
+++ resources/views/pur/seleksi/daftar_supplier/print.blade.php
@@ -1,35 +1,85 @@
 <x-hccp-print :title="$title" :dok="$dok">
-    <span class="text-sm">Tanggal Update :  {{ $datas->last()->updated_at->format('j F Y') }}</span>
-    
-    <table class="table table-xs border-dark table-bordered">
-        <thead>
+    <table>
+        <tr>
+            <th>Tanggal Update</th>
+            <th>: {{ $datas->last()->updated_at->format('j F Y') }}</th>
+        </tr>
+        @if ($k == 'sbw')
             <tr>
-                <th class="head">No</th>
-                <th class="head">Nama Supplier</th>
-                <th class="head">Alamat Supplier</th>
-                <th class="head">Produsen</th>
-                <th class="head">Contact Person</th>
-                <th class="head">No Telp</th>
-                <th class="head">Jenis Produk / Layanan</th>
-                <th class="head">Hasil Evaluasi</th>
-                <th class="head">Keterangan</th>
+                <th>Jenis Supplier</th>
+                <th>: Supplier Material SBW</th>
             </tr>
-        </thead>
-        <tbody>
-            @foreach ($datas as $d)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $d->nama_supplier }}</td>
-                    <td>{{ $d->alamat }}</td>
-                    <td>{{ $d->produsen }}</td>
-                    <td>{{ $d->contact_person }}</td>
-                    <td>{{ $d->no_telp }}</td>
-                    <td>{{ $d->kategori }}</td>
-                    <td>{{ $d->hasil_evaluasi }}</td>
-                    <td>{{ $d->ket }}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</x-hccp-print>
+        @endif
 
+    </table>
+
+    @if ($k == 'satu')
+        <table class="table table-xs border-dark table-bordered">
+            <thead>
+                <tr>
+                    <th class="head">No</th>
+                    <th class="head">Nama Supplier</th>
+                    <th class="head">Alamat Supplier</th>
+                    <th class="head">Produsen</th>
+                    <th class="head">Contact Person</th>
+                    <th class="head">No Telp</th>
+                    <th class="head">Jenis Produk / Layanan</th>
+                    <th class="head">Hasil Evaluasi</th>
+                    <th class="head">Keterangan</th>
+                </tr>
+            </thead>
+            <tbody>
+
+
+                @foreach ($datas as $d)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $d->nama_supplier }}</td>
+                        <td>{{ $d->alamat }}</td>
+                        <td>{{ $d->produsen }}</td>
+                        <td>{{ $d->contact_person }}</td>
+                        <td>{{ $d->no_telp }}</td>
+                        <td>{{ $d->kategori }}</td>
+                        <td>{{ $d->hasil_evaluasi }}</td>
+                        <td>{{ $d->ket }}</td>
+                    </tr>
+                @endforeach
+
+
+            </tbody>
+        </table>
+    @else
+        <table class="table table-xs border-dark table-bordered">
+            <thead>
+                <tr>
+                    <th class="head text-center">No</th>
+                    <th class="head text-center">Nama Supplier</th>
+                    <th class="head text-center">Alamat Supplier</th>
+                    <th class="head text-center">Nama Contact Person</th>
+                    <th class="head text-center">Nomor KTP</th>
+                    <th class="head text-center">No Telp</th>
+                    <th class="head text-center">Hasil Evaluasi Rutin</th>
+                    <th class="head text-center">Keterangan</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($rumah_walet as $d)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $d->nama }}</td>
+                        <td>{{ $d->alamat }}</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+
+</x-hccp-print>
