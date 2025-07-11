19e9c1377278eba1c94c7187d38c0c98d2b325f6
aldmf26 <aldimf26@gmail.com>
2025-07-11 11:31:30 +0800

    das

--- resources/views/ppc/gudang_rm/label_identitas_bahan/print.blade.php
+++ resources/views/ppc/gudang_rm/label_identitas_bahan/print.blade.php
@@ -82,106 +82,20 @@
     $k = request()->get('k');
 @endphp
 
-<body @if ($k == 'sbw') x-data="{ jumlah: 1 }" @endif>
+<body x-data="{ jumlah: 1 }">
     <center>
-        @if ($k == 'sbw')
-            <div class="tidak-cetak p-2">
-                <div class="col-2">
-                    <input type="number" class="form-control form-control-sm" placeholder="isi untuk berapa label"
-                        min="1" x-model.number="jumlah">
-                </div>
+        <div class="tidak-cetak p-2">
+            <div class="col-2">
+                <input type="number" class="form-control form-control-sm" placeholder="isi untuk berapa label"
+                    min="1" x-model.number="jumlah">
             </div>
-        @endif
+        </div>
         <div class="p-2">
             <div class="row">
-                @if ($k == 'sbw')
+
+
+                @foreach ($labels as $d)
                     <template x-for="i in jumlah" :key="i">
-                        @foreach ($labels as $d)
-                            <!-- Label pertama -->
-                            <div class="label mt-3">
-                                <div class="header">
-                                    <div class="d-flex justify-content-evenly">
-                                        <img src="{{ asset('img/logo.jpeg') }}" class="logo" alt="Logo">
-                                        <p>PT. AGRIKAGATYA ARUM</p>
-                                    </div>
-
-                                    <p><strong><u>Identitas Bahan {{ ucwords($d->kategori) }}</u></strong></p>
-                                </div>
-                                <table style="font-size: 10px; text-align: left">
-                                    <tr>
-                                        <td>Nama
-                                            {{ in_array($d->kategori, ['barang', 'kemasan']) ? 'Barang' : 'Bahan Baku' }}
-                                        </td>
-                                        <td>:</td>
-                                        <th>{{ $d->kategori == 'Baku' ? $d->grade : $d->barang->nama_barang }}</th>
-                                    </tr>
-                                    <tr>
-                                        <td>
-                                            Nama Produsen
-                                        </td>
-                                        <td>:</td>
-                                        <td>{{ $d->kategori == 'Baku' ? $d->rumah_walet : $d->supplier->nama_supplier }}
-                                        </td>
-                                    </tr>
-
-                                    <tr>
-                                        <td>Tanggal Kedatangan</td>
-                                        <td>:</td>
-                                        <td>
-                                            @if ($d->kategori == 'Baku')
-                                                @php
-                                                    $tgl_sbw = date('Y-m-d', strtotime('+1 day', strtotime($d->tgl)));
-                                                @endphp
-                                                {{ tanggal($tgl_sbw) }}
-                                            @elseif($d->kategori == 'barang')
-                                                {{ tanggal($d->tanggal_terima) }}
-                                            @else
-                                                {{ tanggal($d->tanggal_penerimaan) }}
-                                            @endif
-                                        </td>
-                                    </tr>
-                                    <tr>
-                                        <td>Kode Lot</td>
-                                        <td>:</td>
-                                        <td>
-                                            {{ $d->kategori == 'Baku' ? $d->no_invoice : $d->kode_lot }}
-                                        </td>
-                                        </td>
-                                    </tr>
-                                    @if ($d->kategori == 'Baku')
-                                        <tr>
-                                            <td>Kode Grading</td>
-                                            <td>:</td>
-                                            <td>
-                                                {{ $d->kategori == 'Baku' ? $d->kode : $d->kode_lot }}
-                                            </td>
-                                        </tr>
-                                    @endif
-
-
-                                    <tr>
-                                        <td>Keterangan</td>
-                                        <td>:</td>
-                                        <td>{{ $d->keterangan }}</td>
-                                    </tr>
-                                </table>
-                                <table class="signature-table">
-                                    <tr>
-                                        <td>KA. Gudang</td>
-                                        <td>QC Incoming</td>
-                                        <td>Status</td>
-                                    </tr>
-                                    <tr>
-                                        <td></td> <!-- Untuk tanda tangan -->
-                                        <td></td>
-                                        <td></td>
-                                    </tr>
-                                </table>
-                            </div>
-                        @endforeach
-                    </template>
-                @else
-                    @foreach ($labels as $d)
                         <!-- Label pertama -->
                         <div class="label mt-3">
                             <div class="header">
@@ -264,8 +178,8 @@
                                 </tr>
                             </table>
                         </div>
-                    @endforeach
-                @endif
+                    </template>
+                @endforeach
 
             </div>
         </div>
