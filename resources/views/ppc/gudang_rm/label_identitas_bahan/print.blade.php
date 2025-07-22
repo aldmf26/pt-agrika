4a254d22d8b7ce08d51161fe70603550c001c26f
nadws <nandw567@gmail.com>
2025-07-22 13:51:04 +0800

    update print.blade.php

--- resources/views/ppc/gudang_rm/label_identitas_bahan/print.blade.php
+++ resources/views/ppc/gudang_rm/label_identitas_bahan/print.blade.php
@@ -35,7 +35,7 @@
 
         .label {
             border: 1px solid #000;
-            padding: 10px;
+            padding: 5px;
             text-align: center;
             width: 30%;
             display: inline-block;
