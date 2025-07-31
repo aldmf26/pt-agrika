28e037260c3663dca201536b957e4a0adffe12bf
nadws <nandw567@gmail.com>
2025-07-31 11:46:00 +0800

    update resources/views/ppc/gudang_fg/kartu_stok/index.blade.php, print.blade.php and nanda.php

--- resources/views/ppc/gudang_fg/kartu_stok/index.blade.php
+++ resources/views/ppc/gudang_fg/kartu_stok/index.blade.php
@@ -1,29 +1,34 @@
 <x-app-layout :title="$title">
-    <table id="example" class="table table-bordered">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Nama Barang</th>
-                <th class="text-end">Pcs</th>
-                <th class="text-end">Gr</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($kartu as $d)
-                <tr>
-                    <td>{{ $loop->iteration }}</td>
-                    <td>{{ $d['grade'] }}</td>
-                    <td>{{ number_format($d['pcs'] - $d['pcs_akhir'], 0) }}</td>
-                    <td>{{ number_format($d['gr'] - $d['gr_akhir'], 0) }}</td>
-                    <td>
-                        <a class="btn btn-xs  btn-primary" target="_blank"
-                            href="{{ route('ppc.gudang-fg.4.print', ['grade' => $d['grade']]) }}"><i
-                                class="fas fa-print"></i></a>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="row">
+        <div class="col-lg-6">
+            <table id="example" class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Nama Barang</th>
+                        <th class="text-end">Pcs</th>
+                        <th class="text-end">Gr</th>
+                        <th>Aksi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($kartu as $d)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $d['grade'] }}</td>
+                            <td>{{ number_format($d['pcs'] - $d['pcs_akhir'], 0) }}</td>
+                            <td>{{ number_format($d['gr'] - $d['gr_akhir'], 0) }}</td>
+                            <td>
+                                <a class="btn btn-xs  btn-primary" target="_blank"
+                                    href="{{ route('ppc.gudang-fg.4.print', ['grade' => $d['grade']]) }}"><i
+                                        class="fas fa-print"></i></a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
 
 </x-app-layout>
