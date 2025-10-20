bd4181f591d053f901076343644e048b1588c0f7
nadws <nandw567@gmail.com>
2025-10-20 08:51:37 +0800

    update Hrga3PermintaanPerbaikan.php, form_permintaanperbaikan.blade.php and resources/views/hrga/hrga5/hrga3_permintaanperbaikan/index.blade.php

--- app/Http/Controllers/Hrga/Hrga5PerbaikandanPerawatanSaranaPrasarana/Hrga3PermintaanPerbaikan.php
+++ app/Http/Controllers/Hrga/Hrga5PerbaikandanPerawatanSaranaPrasarana/Hrga3PermintaanPerbaikan.php
@@ -93,7 +93,7 @@ class Hrga3PermintaanPerbaikan extends Controller
 
 
 
-        return redirect()->route('hrga5.3.index')->with('sukses', 'Permintaan perbaikan berhasil diajukan');
+        return redirect()->route('hrga5.3.sukses')->with('sukses', 'Permintaan perbaikan berhasil diajukan');
     }
     public function sukses(Request $r)
     {
