dcdbccb783a794295fd03a2af81f0d104ce55505
nadws <nandw567@gmail.com>
2025-10-23 08:49:43 +0800

    update resources/views/hrga/hrga3/hrga3usulandanidentifikasi/index.blade.php

--- resources/views/hrga/hrga3/hrga3usulandanidentifikasi/index.blade.php
+++ resources/views/hrga/hrga3/hrga3usulandanidentifikasi/index.blade.php
@@ -29,8 +29,7 @@
                     @foreach ($usulan as $u)
                         <tr>
                             <td>{{ $loop->iteration }}</td>
-                            <td>{{ ucfirst(strtolower($u->pelatihan->materi_pelatihan ?? '-')) }} -
-                                {{ $u->nota_pelatihan }}</td>
+                            <td>{{ ucfirst(strtolower($u->pelatihan->materi_pelatihan ?? '-')) }}</td>
 
                             <td>{{ ucfirst(strtolower($u->pengusul)) }}</td>
 
