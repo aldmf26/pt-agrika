211a59e23a5870599760eaa088d6f78d5fccb653
aldmf26 <aldimf26@gmail.com>
2025-10-24 11:50:28 +0800

    dsa

--- app/Http/Controllers/PPC/Gudang_RM/RM5LabelIdentitasBahanController.php
+++ app/Http/Controllers/PPC/Gudang_RM/RM5LabelIdentitasBahanController.php
@@ -143,7 +143,6 @@ class RM5LabelIdentitasBahanController extends Controller
             }
             if ($identitas === 'kemasan') {
                 $kemasan = PenerimaanKemasanHeader::with(['barang', 'supplier'])->where('kode_lot', $id)->first();
-                dd($kemasan);
                 if ($kemasan) {
                     // Ensure supplier is an object
                     $kemasan->supplier = $kemasan->barang->supplier->nama_supplier ?? (object)['nama_supplier' => '-'];
@@ -165,6 +164,8 @@ class RM5LabelIdentitasBahanController extends Controller
             }
             if ($identitas === 'barang') {
                 $kemasan = PenerimaanHeader::with(['barang', 'supplier'])->where('kode_lot', $id)->first();
+                dd($kemasan);
+
                 if ($kemasan) {
                     // Ensure supplier is an object
                     $kemasan->supplier = $kemasan->barang->supplier->nama_supplier ?? (object)['nama_supplier' => '-'];
