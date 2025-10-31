bf5de536e90e5a02f8ee85f77285e1e00de7c88d
nadws <nandw567@gmail.com>
2025-10-31 16:16:00 +0800

    update print.blade.php

--- resources/views/hrga/hrga6/hrga2_ceklist_sanitasi/print.blade.php
+++ resources/views/hrga/hrga6/hrga2_ceklist_sanitasi/print.blade.php
@@ -38,13 +38,15 @@
                                             str_pad($bulan, 2, '0', STR_PAD_LEFT) .
                                             '-' .
                                             str_pad($h['tanggal'], 2, '0', STR_PAD_LEFT);
+                                        $hariKe = date('w', strtotime($tgl)); // 0 = Minggu, 1 = Senin, dst
                                     @endphp
-                                    @if ($tgl <= date('Y-m-d'))
+
+                                    @if ($tgl <= date('Y-m-d') && $hariKe != 0)
                                         ✓
-                                    @else
                                     @endif
                                 </td>
                             @endforeach
+
                         </tr>
                     @endforeach
                     <tr>
