1bf793edddf25f6f00a0e9f96d5911ced59daf29
nadws <nandw567@gmail.com>
2025-11-03 14:30:05 +0800

    update print.blade.php

--- resources/views/hrga/hrga3/hrga3usulandanidentifikasi/print.blade.php
+++ resources/views/hrga/hrga3/hrga3usulandanidentifikasi/print.blade.php
@@ -102,7 +102,7 @@
                             <tr>
                                 <td class="text-center">{{ $loop->iteration }}</td>
                                 <td>{{ ucwords(strtolower($u->data_pegawai->nama ?? '-')) }}</td>
-                                <td>{{ $u->data_pegawai->nik }}</td>
+                                <td>{{ $u->data_pegawai->nik ?? '-' }}</td>
                                 <td>{{ $u->data_pegawai->divisi->divisi ?? '-' }}</td>
                                 <td>{{ ucwords(strtolower($u->pengusul)) }}</td>
                                 <td>{{ ucfirst(strtolower($u->usulan_jenis_pelatihan)) }}</td>
