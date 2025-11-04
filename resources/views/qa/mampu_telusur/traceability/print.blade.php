739a90882249b0ac827c2a6582217acb89116cf4
nadws <nandw567@gmail.com>
2025-11-04 10:56:54 +0800

    update print.blade.php

--- resources/views/qa/mampu_telusur/traceability/print.blade.php
+++ resources/views/qa/mampu_telusur/traceability/print.blade.php
@@ -334,6 +334,10 @@
                             @endforeach
 
                         </tbody>
+                        <tfoot>
+                            <th>{{ sumBk('pcs', $kirim) }}</th>
+                            <th>{{ sumBk('gr', $kirim) }}</th>
+                        </tfoot>
 
                     </table>
                 </div>
