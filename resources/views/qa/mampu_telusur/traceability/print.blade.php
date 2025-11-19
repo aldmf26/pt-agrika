2acdf49250ef54d6848de23693afb110097b8f3e
nadws <nandw567@gmail.com>
2025-11-19 09:53:58 +0800

    update print.blade.php

--- resources/views/qa/mampu_telusur/traceability/print.blade.php
+++ resources/views/qa/mampu_telusur/traceability/print.blade.php
@@ -223,10 +223,10 @@
                                         {{ $b['pcs_akhir'] != $b['pcs_awal'] ? '-' : number_format($b['gr_akhir'], 0) }}
                                     </td>
                                     <td class="text-end">
-                                        {{ $b['pcs_akhir'] != $b['pcs_awal'] ? '-' : tanggal($b['tgl_serah'] ?? '-') }}
+                                        {{ $b['pcs_akhir'] != $b['pcs_awal'] ? '-' : $b['tgl_serah'] }}
                                     </td>
                                     <td class="text-end">
-                                        {{ $b['pcs_awal_ctk'] == 0 ? '-' : tanggal($b['tgl_selesai_ctk']) }}
+                                        {{ $b['pcs_awal_ctk'] == 0 ? '-' : $b['tgl_selesai_ctk'] }}
                                     </td>
 
                                     <td class="text-end">
@@ -272,7 +272,7 @@
                                         $tgl = date('Y-m-d', strtotime($b['tgl_grading'] . ' + 1 days'));
                                     @endphp
                                     <td class="text-end">
-                                        {{ $b['pcs_grading'] != $b['pcs_awal'] ? '-' : tanggal($tgl) }}
+                                        {{ $b['pcs_grading'] != $b['pcs_awal'] ? '-' : $tgl }}
                                     </td>
                                     {{-- <td></td>
                                     <td></td>
