9774f213f9aba9d7ae05da606392dbf0e0447130
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2025-11-20 11:56:09 +0800

    Add format prop to QrCode generation in ttd-barcode component

--- resources/views/components/ttd-barcode.blade.php
+++ resources/views/components/ttd-barcode.blade.php
@@ -1,8 +1,9 @@
 @props([
     'size' => '80',
     'id_pegawai' => null,
+    'format' => 'png',
 ])
 <span>
     {{-- <img src="data:image/png;base64,{!! base64_encode(QrCode::format('png')->size($size)->generate(route('verify-ttd', $id_pegawai))) !!}"> --}}
-    {{ QrCode::size($size)->generate(route('verify-ttd', $id_pegawai)) }}
+    {{ QrCode::format($format)->size($size)->generate(route('verify-ttd', $id_pegawai)) }}
 </span>
