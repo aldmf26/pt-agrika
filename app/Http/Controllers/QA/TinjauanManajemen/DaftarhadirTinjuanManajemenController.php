b7485a038e463f37635092765845cfd87c7293de
nadws <nandw567@gmail.com>
2025-11-20 09:44:39 +0800

    update DaftarhadirTinjuanManajemenController.php

--- app/Http/Controllers/QA/TinjauanManajemen/DaftarhadirTinjuanManajemenController.php
+++ app/Http/Controllers/QA/TinjauanManajemen/DaftarhadirTinjuanManajemenController.php
@@ -44,7 +44,7 @@ class DaftarhadirTinjuanManajemenController extends Controller
 
         $agenda = DB::table('daftar_hadir')
             ->leftJoin('data_pegawais', 'data_pegawais.karyawan_id_dari_api', '=', 'daftar_hadir.pegawai_id')
-            ->where('nota_agenda', $r->nota_agenda)->get();
+            ->where('nota_agenda', $r->nota_agenda)->orderBy('data_pegawais.nama', 'asc')->get();
 
         $data = [
             'title' => 'Daftar Hadir Tinjauan Manajemen',
