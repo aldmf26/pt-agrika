1a1507a1b49ff02a9e1ab17673d50af7ba74b86f
Fahrizaldi <45739897+aldmf26@users.noreply.github.com>
2026-01-10 09:33:25 +0800

    nama supplier label print edit

--- resources/views/ppc/gudang_rm/label_identitas_bahan/print.blade.php
+++ resources/views/ppc/gudang_rm/label_identitas_bahan/print.blade.php
@@ -150,7 +150,8 @@
                                         Nama Produsen
                                     </td>
                                     <td>:</td>
-                                    <td>{{ $d->kategori == 'Baku' ? ucwords($d->rumah_walet) : $d->supplier }}
+                                    <td>{{ $d->kategori == 'Baku' ? ucwords($d->rumah_walet) : $d->supplier->nama_supplier }}
+                                    </td>
                                     </td>
                                     @if ($d->kategori == 'Baku')
                                         <td>&nbsp;</td>
