ae255babcaeb5a37d69d08cf07f8fce7ccea4399
nadws <nandw567@gmail.com>
2026-01-14 08:23:22 +0800

    update print.blade.php

--- resources/views/qa/mampu_telusur/traceability/print.blade.php
+++ resources/views/qa/mampu_telusur/traceability/print.blade.php
@@ -211,7 +211,7 @@
                                         <td></td>
                                     @endif
                                     <td class="text-end">
-                                        {{ number_format($b['gr_awal'], 0) }}
+                                        {{ number_format($b['gr_awal'], 0) }} dsa
                                     </td>
                                     <td class="text-end">{{ tanggal($b['tgl_terima'] ?? '-') }}</td>
                                     <td class="text-end">{{ number_format($b['pcs_awal'], 0) }} /
